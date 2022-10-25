ddcb7993ce5e23c2b98f3f966750da2632b0823f
Carlos Munoz <42192364+codewithcarlos@users.noreply.github.com>
2022-10-25 12:22:44 -0700

    Create Java leetcode repo

---
+++ 1662.java
@@ -0,0 +1,56 @@
+// https://leetcode.com/problems/check-if-two-string-arrays-are-equivalent/
+
+// Solution1: Without StringBuilder
+// Notes: 
+// - when initalizing to empty string have to use double quotations. can't use single quotes or you get empty character literal error
+// - after concatenating strings, can't use == to compare strings. have to use the equals method
+
+class Solution1 {
+    public boolean arrayStringsAreEqual(String[] word1, String[] word2) {
+        String w1 = "";
+        String w2 = "";
+        for (String s : word1) {
+            w1 += s;
+        }
+        for (String s : word2) {
+            w2 += s;
+        }
+        return w1.equals(w2);
+    }
+}
+
+// Solution2: With StringBuilder
+// Notes:
+// - use append to add letters
+// - use compareTo == 0 OR use toString() and equals()
+class Solution2 {
+    public boolean arrayStringsAreEqual(String[] word1, String[] word2) {
+        StringBuilder w1 = new StringBuilder();
+        StringBuilder w2 = new StringBuilder();
+        for (String s : word1) {
+            w1.append(s);
+        }
+        for (String s : word2) {
+            w2.append(s);
+        }
+        return w1.compareTo(w2) == 0;
+        // return w1.toString().equals(w2.toString());
+    }
+}
+
+// Example Javascript solution
+// /**
+// * @param {string[]} word1
+// * @param {string[]} word2
+// * @return {boolean}
+// */
+// const arrayStringsAreEqual = function(word1, word2) {
+// let w1 = '', w2 = '';
+// for (let word of word1) {
+// w1 += word;
+// }
+// for (let word of word2) {
+// w2 += word;
+// }
+// return w1 === w2;
+// };
---
+++ README.md
@@ -0,0 +1,4 @@
+# Solving Leetcode problems in Java
+
+Solving problems in Java to get a better understanding of the syntax
+Plan is to do Leetcode problem of the day and other study plan problems
